489ab5f3583230093488849f8969d44d424f2fb3
Aren Leishman <aren.leishman@gmail.com>
2024-01-06 17:17:12 +0800

    Do some tweaking so CAN flashing works slightly better

--- STM32/flash_can.sh
+++ STM32/flash_can.sh
@@ -1,12 +1,14 @@
 cansend can0 005# 
-sleep 0.1
-cansend can0 079#00 
-sleep 0.1
-cansend can0 092#00
-sleep 0.1
-cansend can0 073#00
-sleep 0.1
+#sleep 0.1
+#cansend can0 079#00 
+#sleep 0.1
+#cansend can0 092#00
+#sleep 0.1
+#cansend can0 073#00
+#sleep 0.1
+
+pushd /home/aren/Documents/Code/can-prog/canprog
+export PYTHONPATH="/home/aren/Documents/Code/can-prog/:$PYTHONPATH"
 /home/aren/Documents/Code/can-prog/venv/bin/python /home/aren/Documents/Code/can-prog/canprog/main.py -n can0 stm32 erase
-/home/aren/Documents/Code/can-prog/venv/bin/python /home/aren/Documents/Code/can-prog/canprog/main.py -n can0 stm32 write -v /home/aren/Dropbox/Dropbox/DiveCANHeadRev2/STM32/build/STM32.hex 
-echo "Setting options and reset"
-cansend can0 031#1FFF780004 && cansend can0 004#FFEFF8AA
+/home/aren/Documents/Code/can-prog/venv/bin/python /home/aren/Documents/Code/can-prog/canprog/main.py -n can0 stm32 write -g /home/aren/Dropbox/DiveCANHeadRev2/STM32/build/STM32.hex 
+popd
