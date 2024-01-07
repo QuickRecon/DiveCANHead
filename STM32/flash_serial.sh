37b4d6356ac8db23f7d5ea228c3f58d66cb4e7fd
Aren Leishman <aren.leishman@gmail.com>
2024-01-07 17:59:37 +0800

    Add can logic to serial flash script to save having to push the boot buttons

--- STM32/flash_serial.sh
+++ STM32/flash_serial.sh
@@ -1,3 +1,11 @@
+sudo ip link set can0 down
+sudo ip link set can0 txqueuelen 100
+sudo ip link set can0 type can bitrate 125000 sjw 2 #tdcv 0 tdco 0 tdcf 0 restart-ms 0
+sudo ip link set can0 up
+sleep 0.1
+
+cansend can0 079# 
+sleep 0.1
 
 killall screen
 stm32flash -vR -b 115200 -w build/STM32.hex /dev/ttyUSB0 && screen /dev/ttyUSB0 19200
