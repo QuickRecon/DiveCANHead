51a2e8c4a885602bc65d779f2cff1da4390d4be8
Aren Leishman <aren.leishman@gmail.com>
2024-04-07 12:39:54 +0800

    fix the can setup script to reflect how to actually set up the canbus

--- STM32/setup_can.sh
+++ STM32/setup_can.sh
@@ -1,2 +1,4 @@
-ip link set can0 type can bitrate 125000
-ip link set can0 up
+sudo ip link set can0 down
+sudo ip link set can0 txqueuelen 100
+sudo ip link set can0 type can bitrate 125000 sjw 2 #tdcv 0 tdco 0 tdcf 0 restart-ms 0
+sudo ip link set can0 up
