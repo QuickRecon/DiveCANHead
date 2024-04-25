27237588595a6bf081bebb0d6a1db535976d3244
Aren Leishman <aren.leishman@gmail.com>
2024-04-25 09:48:51 +0800

    Clean up some sonar warns

--- STM32/Core/Src/Hardware/log.c
+++ STM32/Core/Src/Hardware/log.c
@@ -4,19 +4,19 @@
 
 extern SD_HandleTypeDef hsd1;
 
-// SD card driver overrides to make the DMA work properly
+/* SD card driver overrides to make the DMA work properly */
 uint8_t BSP_SD_WriteBlocks_DMA(uint32_t *pData, uint32_t WriteAddr, uint32_t NumOfBlocks)
 {
     uint8_t sd_state = MSD_OK;
-    // Since we are only using 1 DMA channel for SDIO
-    // Change DMA direction before calling SD Read
-    // Direction can only be changed when DMA is disabled
+    /* Since we are only using 1 DMA channel for SDIO
+     * Change DMA direction before calling SD Read
+     * Direction can only be changed when DMA is disabled */
 
     __HAL_DMA_DISABLE(hsd1.hdmatx);
 
     hsd1.hdmatx->Init.Direction = DMA_MEMORY_TO_PERIPH;
 
-    hsd1.hdmatx->Instance->CCR |= (uint32_t)DMA_CCR_DIR;
+    hsd1.hdmatx->Instance->CCR |= DMA_CCR_DIR;
 
     /* Write block(s) in DMA transfer mode */
     if (HAL_SD_WriteBlocks_DMA(&hsd1, (uint8_t *)pData, WriteAddr, NumOfBlocks) != HAL_OK)
@@ -31,9 +31,9 @@ uint8_t BSP_SD_ReadBlocks_DMA(uint32_t *pData, uint32_t ReadAddr, uint32_t NumOf
 {
     uint8_t sd_state = MSD_OK;
 
-    // Since we are only using 1 DMA channel for SDIO
-    // Change DMA direction before calling SD Read
-    // Direction can only be changed when DMA is disabled
+    /* Since we are only using 1 DMA channel for SDIO
+     * Change DMA direction before calling SD Read
+     * Direction can only be changed when DMA is disabled */
 
     __HAL_DMA_DISABLE(hsd1.hdmarx);
 
@@ -75,7 +75,7 @@ void InitLog(void)
         {
             /*  Write to the text file */
             res = f_write(&SDFile, wtext, strlen((char *)wtext), (void *)&byteswritten);
-            if ((byteswritten == 0) || (res != FR_OK))
+            if ((0 == byteswritten) || (res != FR_OK))
             {
                 Error_Handler();
                 serial_printf("bytes error %d\r\n", res);
