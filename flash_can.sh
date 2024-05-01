e4f332630eb620fcf0413f0271e0a512ecab4712
Aren Leishman <aren.leishman@gmail.com>
2024-05-01 16:09:09 +0800

    Update paths in can flash script

--- flash_can.sh
+++ flash_can.sh
@@ -5,8 +5,8 @@ sudo ip link set can0 up
 sleep 0.1
 
 cansend can0 079# 
-sleep 0.1
+sleep 0.5
 pushd /home/aren/Documents/Code/can-prog/canprog
 export PYTHONPATH="/home/aren/Documents/Code/can-prog/:$PYTHONPATH"
-/home/aren/Documents/Code/can-prog/venv/bin/python /home/aren/Documents/Code/can-prog/canprog/main.py -n can0 stm32 write -evg /home/aren/Dropbox/DiveCANHeadRev2/STM32/build/STM32.hex 
+/home/aren/Documents/Code/can-prog/.venv/bin/python /home/aren/Documents/Code/can-prog/canprog/main.py -n can0 stm32 write -evg /home/aren/Dropbox/DiveCANHeadRev2/STM32/build/STM32.hex 
 popd
