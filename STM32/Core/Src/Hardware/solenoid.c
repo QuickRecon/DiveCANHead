a920f0c6b50fbf9bf7ff5bf094737eaa12a39e75
QuickRecon <aren.leishman@gmail.com>
2024-08-02 10:09:40 +0800

    Specify void parameter list

--- STM32/Core/Src/Hardware/solenoid.c
+++ STM32/Core/Src/Hardware/solenoid.c
@@ -1,7 +1,7 @@
 #include "solenoid.h"
 #include "main.h"
 
-void setSolenoidOn()
+void setSolenoidOn(void)
 {
     HAL_GPIO_WritePin(SOL_DIS_BATT_GPIO_Port, SOL_DIS_BATT_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(SOL_DIS_CAN_GPIO_Port, SOL_DIS_CAN_Pin, GPIO_PIN_RESET);
@@ -9,7 +9,7 @@ void setSolenoidOn()
     HAL_GPIO_WritePin(SOLENOID_GPIO_Port, SOLENOID_Pin, GPIO_PIN_SET);
 }
 
-void setSolenoidOff()
+void setSolenoidOff(void)
 {
     HAL_GPIO_WritePin(SOL_DIS_BATT_GPIO_Port, SOL_DIS_BATT_Pin, GPIO_PIN_SET);
     HAL_GPIO_WritePin(SOL_DIS_CAN_GPIO_Port, SOL_DIS_CAN_Pin, GPIO_PIN_SET);
