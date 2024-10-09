d683225b6bd147e41239b0537f90f5c2a92dd0c5
QuickRecon <aren.leishman@gmail.com>
2024-10-09 09:51:31 +0800

    Add ignores to python type checking

--- HW Testing/Tests/psu.py
+++ HW Testing/Tests/psu.py
@@ -8,14 +8,14 @@ CAN_PWR_PORT = '/dev/ttyPSU2'
 def setOff():
     """ Set the power to off to ensure a reset """
     try:
-        battery = Riden(BATTERY_PORT, 115200, address = 1)
+        battery = Riden(BATTERY_PORT, 115200, address = 1) # type: ignore
         battery.set_output(False)
     except Exception:
         # We don't really care, we tried
         print("Failed to power off battery supply")
 
     try:
-        can_pwr = Riden(CAN_PWR_PORT, 115200, address = 1)
+        can_pwr = Riden(CAN_PWR_PORT, 115200, address = 1) # type: ignore
         can_pwr.set_output(False)
     except Exception:
         # We don't really care if this fails
@@ -25,14 +25,14 @@ def setOff():
 def setDefaultPower():
     """ Set a sane default power configuration for tests that aren't explicitly looking at power behavior """
     try:
-        battery = Riden(BATTERY_PORT, 115200, address = 1)
+        battery = Riden(BATTERY_PORT, 115200, address = 1) # type: ignore
         battery.set_output(False)
     except Exception:
         # We don't really care, we tried
         print("Failed to configure default battery supply")
 
     try:
-        can_pwr = Riden(CAN_PWR_PORT, 115200, address = 1)
+        can_pwr = Riden(CAN_PWR_PORT, 115200, address = 1) # type: ignore
         can_pwr.set_v_set(5.0)
         can_pwr.set_i_set(0.1)
         can_pwr.set_i_set(0.1)
@@ -45,8 +45,8 @@ class PSU(object):
     """ Class for interacting with 2 RIDEN PSUs for simulating various power scenarios """
     def __init__(self) -> None:
         try:
-            self._battery = Riden(BATTERY_PORT, 115200, address = 1)
-            self._can_pwr = Riden(CAN_PWR_PORT, 115200, address = 1)
+            self._battery = Riden(BATTERY_PORT, 115200, address = 1) # type: ignore
+            self._can_pwr = Riden(CAN_PWR_PORT, 115200, address = 1) # type: ignore
 
             self._battery.set_v_set(5.0)
             self._can_pwr.set_v_set(5.0)
