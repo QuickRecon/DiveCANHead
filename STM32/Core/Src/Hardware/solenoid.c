e7b7c0fbb642b2274f4f0aac99c7884628aa93bc
Aren Leishman <aren.leishman@gmail.com>
2025-11-17 20:38:46 +0800

    Refactor solenoid method to clear a linter warning

--- STM32/Core/Src/Hardware/solenoid.c
+++ STM32/Core/Src/Hardware/solenoid.c
@@ -14,30 +14,38 @@
  */
 void setSolenoidOn(PowerSelectMode_t powerMode)
 {
-    if(powerMode == MODE_OFF){
+    if (powerMode == MODE_OFF)
+    {
         NON_FATAL_ERROR(SOLENOID_DISABLED_ERR);
-        return;
     }
-    else if(powerMode == MODE_CAN){
-        /* Only enable CAN power */
-        HAL_GPIO_WritePin(SOL_DIS_BATT_GPIO_Port, SOL_DIS_BATT_Pin, GPIO_PIN_SET);
-        HAL_GPIO_WritePin(SOL_DIS_CAN_GPIO_Port, SOL_DIS_CAN_Pin, GPIO_PIN_RESET);
-    } else if(powerMode == MODE_BATTERY){
-        /* Only enable Battery power */
-        HAL_GPIO_WritePin(SOL_DIS_BATT_GPIO_Port, SOL_DIS_BATT_Pin, GPIO_PIN_RESET);
-        HAL_GPIO_WritePin(SOL_DIS_CAN_GPIO_Port, SOL_DIS_CAN_Pin, GPIO_PIN_SET);
-    } else {
-        /* Enable both power sources, pull from Battery then CAN */
-        HAL_GPIO_WritePin(SOL_DIS_BATT_GPIO_Port, SOL_DIS_BATT_Pin, GPIO_PIN_RESET);
-        /* We need to wait for the settling time to ensure we draw the startup surge *only* from Battery, otherwise the shearwater pulls the plug on us and we use our reserve before the switch */
-        osDelay(TIMEOUT_5MS_TICKS);
-        HAL_GPIO_WritePin(SOL_DIS_CAN_GPIO_Port, SOL_DIS_CAN_Pin, GPIO_PIN_RESET);
+    else
+    {
+        if (powerMode == MODE_CAN)
+        {
+            /* Only enable CAN power */
+            HAL_GPIO_WritePin(SOL_DIS_BATT_GPIO_Port, SOL_DIS_BATT_Pin, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(SOL_DIS_CAN_GPIO_Port, SOL_DIS_CAN_Pin, GPIO_PIN_RESET);
+        }
+        else if (powerMode == MODE_BATTERY)
+        {
+            /* Only enable Battery power */
+            HAL_GPIO_WritePin(SOL_DIS_BATT_GPIO_Port, SOL_DIS_BATT_Pin, GPIO_PIN_RESET);
+            HAL_GPIO_WritePin(SOL_DIS_CAN_GPIO_Port, SOL_DIS_CAN_Pin, GPIO_PIN_SET);
+        }
+        else
+        {
+            /* Enable both power sources, pull from Battery then CAN */
+            HAL_GPIO_WritePin(SOL_DIS_BATT_GPIO_Port, SOL_DIS_BATT_Pin, GPIO_PIN_RESET);
+            /* We need to wait for the settling time to ensure we draw the startup surge *only* from Battery, otherwise the shearwater pulls the plug on us and we use our reserve before the switch */
+            (void)osDelay(TIMEOUT_5MS_TICKS);
+            HAL_GPIO_WritePin(SOL_DIS_CAN_GPIO_Port, SOL_DIS_CAN_Pin, GPIO_PIN_RESET);
+        }
+
+        HAL_GPIO_WritePin(SOLENOID_GPIO_Port, SOLENOID_Pin, GPIO_PIN_SET);
+
+        HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, GPIO_PIN_SET);
+        serial_printf("Solenoid ON");
     }
-
-    HAL_GPIO_WritePin(SOLENOID_GPIO_Port, SOLENOID_Pin, GPIO_PIN_SET);
-
-    HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, GPIO_PIN_SET);
-    serial_printf("Solenoid ON");
 }
 
 /**
