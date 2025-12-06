0f2e3cd11dcc95fd302e6fcdb94177b9e1404e7f
Aren Leishman <aren.leishman@gmail.com>
2025-12-06 19:17:10 +0800

    Fix unneeded bool literal

--- STM32/Core/Src/Hardware/flash.c
+++ STM32/Core/Src/Hardware/flash.c
@@ -173,7 +173,7 @@ static bool WriteInt32(uint16_t addr, uint32_t value)
     EE_Status result = EE_OK;
     do
     {
-        if (checkCoreVoltage() == false)
+        if (checkCoreVoltage())
         {
             NON_FATAL_ERROR(VCC_UNDER_VOLTAGE_ERR);
         }
